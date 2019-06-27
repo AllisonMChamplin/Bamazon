b2172bb0ff5cb14d8e4bb2389b19443819e2d375
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-27 11:34:40 -0500

    Added manager view products function

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
     user: "root",
     password: "pinkSparkleUnikitty",
     database: "bamazon"
-  });
-  
+});
+
 connection.connect(function (err) {
     if (err) throw err;
     mainMenu();
@@ -22,29 +22,29 @@ function mainMenu() {
             message: "What would you like to do?",
             choices: [
                 "View Products for Sale",
-                // "View Low Inventory",
-                // "Add to Inventory",
-                // "Add New Product",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product",
                 "exit"
             ]
         })
         .then(function (answer) {
             switch (answer.action) {
                 case "View Products for Sale":
-                    productSearch();
+                    viewProducts();
                     break;
 
-                // case "View Low Inventory":
-                //     multiSearch();
-                //     break;
+                case "View Low Inventory":
+                    viewLowInventory();
+                    break;
 
-                // case "Add to Inventory":
-                //     rangeSearch();
-                //     break;
+                case "Add to Inventory":
+                    addToInventory();
+                    break;
 
-                // case "Add New Product":
-                //     songSearch();
-                //     break;
+                case "Add New Product":
+                    addNewProduct();
+                    break;
 
                 case "exit":
                     connection.end();
@@ -53,21 +53,33 @@ function mainMenu() {
         });
 }
 
-function productSearch() {
-    inquirer
-      .prompt({
-        name: "product",
-        type: "input",
-        message: "What product would you like to search for?"
-      })
-      .then(function(answer) {
-        var query = "SELECT position, song, year FROM top5000 WHERE ?";
-        connection.query(query, { product: answer.product }, function(err, res) {
-          if (err) throw err;
-          for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-          }
-          mainMenu();
-        });
-      });
-  }
+// function viewProducts() {
+//     inquirer
+//         .prompt({
+//             name: "product",
+//             type: "input",
+//             message: "What product would you like to search for?"
+//         })
+//         .then(function (answer) {
+//             var query = "SELECT position, song, year FROM top5000 WHERE ?";
+//             connection.query(query, { product: answer.product }, function (err, res) {
+//                 if (err) throw err;
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+//                 }
+//                 mainMenu();
+//             });
+//         });
+// }
+
+function viewProducts() {
+    console.log("\nDisplaying all products...\n");
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        for (var i = 0; i < results.length; i++) {
+            console.log("$" + results[i].price + " || " + results[i].product_name + " || " + results[i].item_id + " || " + " Units: " + results[i].stock_quantity);
+        }
+        console.log("\n");
+        mainMenu();
+    });
+}

4506955ae9dd9846612b9be9f4c074230b1792e3
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-26 21:34:35 -0500

    Set up first function to display current inventory to customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "pinkSparkleUnikitty",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    readProducts();
+});
+
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+    });
+}

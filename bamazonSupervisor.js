0a0ef2a4b8b79dd41952f8fd5e1d01f482534e35
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-27 22:56:44 -0500

    Removed a command line table formatting package

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,15 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-const cTable = require('console.table');
-console.table([
-    {
-        name: 'foo',
-        age: 10
-    }, {
-        name: 'bar',
-        age: 20
-    }
-]);
+var Table = require('easy-table')
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -58,14 +49,11 @@ function viewProductSalesbyDepartment() {
     connection.query(
         "SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.department_name, SUM(products.product_sales) AS product_sales FROM departments INNER JOIN products ON departments.department_name = products.department_name GROUP BY departments.department_name", function (err, res) {
             if (err) throw err;
-            // Log all results of the SELECT statement
-            // console.log(res);
             console.log("\n");
-            console.table(res);
+            console.log(res);
             console.log("\n");
             console.log("\n");
             mainMenu();
-
             // connection.end();
         });
 }
